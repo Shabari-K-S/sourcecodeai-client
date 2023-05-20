d69b9f165210d2d8e9283fa554056a14a5406cf8
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-05-20 07:03:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react'
-import './App.css'
-import './normalize.css'
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+import './normalize.css';
 import MessageWithCode from './MessageWithCode';
-
+import './test.css'; // Import the CSS file for styling
 
 function App() {
-
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [chatLog, setChatLog] = useState([
     {
       "user" : "me",
@@ -17,21 +16,36 @@ function App() {
       "message": "Warm greetings! I am thrilled to introduce myself as your AI assistant, created by Shabari and integrated with the power of Bing. My mission is to serve as your dependable source of information, your trusted problem-solving companion, and your knowledgeable guide. With the combined expertise of Shabari and the vast resources of Bing, I am equipped to assist you with a wide range of topics. Whether you require assistance with intricate challenges, seek guidance on specific subjects, or simply desire an enjoyable conversation, I am here to cater to your needs. Don't hesitate to ask me anything that comes to mind, as I am committed to providing you with comprehensive and valuable responses. Your satisfaction is my utmost priority!"
     }
   ]);
+  const [isLoading, setIsLoading] = useState(false);
+  const chatFormRef = useRef(null);
+  const chatLogRef = useRef(null);
 
-  //clear chat log
-
+  // clear chat log
   function clearChatLog() {
-    setChatLog([]);
+    setChatLog([
+      {
+        "user" : "me",
+        "message": "Who are you?"
+      },
+      {
+        "user" : "gpt",
+        "message": "Warm greetings! I am thrilled to introduce myself as your AI assistant, created by Shabari and integrated with the power of Bing. My mission is to serve as your dependable source of information, your trusted problem-solving companion, and your knowledgeable guide. With the combined expertise of Shabari and the vast resources of Bing, I am equipped to assist you with a wide range of topics. Whether you require assistance with intricate challenges, seek guidance on specific subjects, or simply desire an enjoyable conversation, I am here to cater to your needs. Don't hesitate to ask me anything that comes to mind, as I am committed to providing you with comprehensive and valuable responses. Your satisfaction is my utmost priority!"
+      }
+    ]);
   }
-
-
+  
   async function handleSubmit(e) {
     e.preventDefault();
-    const chatLogNew = [...chatLog, { user: "me", message: input }];
-    await setChatLog([...chatLogNew]);
-    await setInput("");
-    
-    const messages = chatLogNew.map((message) => message.message);
+
+    if (input.trim() === '') {
+      return; // Skip if input is empty or only whitespace
+    }
+
+    const chatLogNew = [...chatLog, { user: 'me', message: input }];
+    setChatLog(chatLogNew);
+    setInput('');
+    setIsLoading(true);
+
     try {
       const response = await fetch(`https://sourcecode-ai.onrender.com/api`, {
         method: 'POST',
@@ -42,43 +56,62 @@ function App() {
           user_input: input,
         }),
       });
+
       if (response.ok) {
         const data = await response.json();
-        await setChatLog([...chatLogNew, { user: "gpt", message: data.response }]);
+        setChatLog([...chatLogNew, { user: 'gpt', message: data.response }]);
       } else {
-        console.error("ResponseError:", response.status);
+        console.error('ResponseError:', response.status);
       }
     } catch (error) {
-      console.error("CodeError:", error);
+      console.error('CodeError:', error);
+    } finally {
+        setIsLoading(false);
+        if (chatFormRef.current) {
+          chatFormRef.current.querySelector('.chat-input-textarea').focus();
+        }
     }
   }
-  
-  
+
+  function scrollChatLog() {
+    if (chatLogRef.current) {
+      chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
+    }
+  }
+
   return (
-    <div className='App'>
-      <aside className='sidemenu'>
-        <div className='side-menu-button' onClick={clearChatLog}>
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button" onClick={clearChatLog}>
           <span>+</span>
           New Chat
         </div>
       </aside>
-      <section className='chatarea'>
-        <div className='chat-log'>
-          {
-            chatLog.map((message, index) => (
-              <ChatMessage key={index} message={message}/>
-            ))
-          }
+      <section className="chatarea">
+        <div className="chat-log" ref={chatLogRef}>
+          {chatLog.map((message, index) => (
+            <ChatMessage key={index} message={message} />
+          ))}
         </div>
-        <form className='chat-form' onSubmit={handleSubmit}>
-          <div className='chat-input-holder'>
-            <input className='chat-input-textarea' rows='1' value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type your prompt here...."></input>
+        <form className="chat-form" onSubmit={handleSubmit} ref={chatFormRef}>
+          <div className="chat-input-holder">
+            <input
+              className="chat-input-textarea"
+              rows="1"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            ></input>
+            <div className="ani">{isLoading && <LoadingAnimation />}</div>
           </div>
         </form>
       </section>
     </div>
-  )
+  );
 }
+
+const LoadingAnimation = () => {
+  return <div className="loading-animation"><span></span><span></span><span></span></div>;
+};
 
 const ChatMessage = ({ message }) => {
   return (
@@ -87,11 +120,11 @@ const ChatMessage = ({ message }) => {
         <div className="chat-message-center">
           <div className={`avatar ${message.user === 'gpt' && 'chatbot'}`}>
             {message.user === 'gpt' && (
-              <img 
-                width="40" 
-                height="40" 
-                src="https://img.icons8.com/3d-fluency/94/chatbot.png" 
-                alt="chatbot"
+              <img
+                width="40"
+                height="40"
+                src="https://img.icons8.com/color/48/artificial-intelligence.png"
+                alt="artificial-intelligence"
               />
             )}
             {message.user === 'me' && (
@@ -104,7 +137,7 @@ const ChatMessage = ({ message }) => {
             )}
           </div>
           <div className="chat-message-content">
-              <MessageWithCode message={message.message} />
+            <MessageWithCode message={message.message} />
           </div>
         </div>
       </div>
@@ -113,4 +146,6 @@ const ChatMessage = ({ message }) => {
 };
 
 
-export default App
+
+
+export default App;
